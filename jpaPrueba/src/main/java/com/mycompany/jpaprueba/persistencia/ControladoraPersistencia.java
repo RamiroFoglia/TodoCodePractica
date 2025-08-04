a988d66ec50713f886fe6b2c7689d338189deee1
Rama87 <128384755+RamiroFoglia@users.noreply.github.com>
2025-08-04 11:59:01 -0300

    Add files via upload
    jpa Todocode

---
+++ jpaPrueba/src/main/java/com/mycompany/jpaprueba/persistencia/ControladoraPersistencia.java
@@ -0,0 +1,9 @@
+package com.mycompany.jpaprueba.persistencia;
+
+/**
+ *
+ * @author Ramiro Foglia
+ */
+public class ControladoraPersistencia {
+    AlumnoJpaController alumJpa = new AlumnoJpaController();
+}
